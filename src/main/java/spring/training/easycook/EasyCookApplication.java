b329fb8b9f80802a2dc6e6447e54433f1a5bf45b
Софья Финько <s.finkoo@yandex.ru>
2022-12-06 20:53:29 +0300

    init

---
+++ src/main/java/spring/training/easycook/EasyCookApplication.java
@@ -0,0 +1,13 @@
+package spring.training.easycook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyCookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyCookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/spring/training/easycook/EasyCookApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.training.easycook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyCookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
